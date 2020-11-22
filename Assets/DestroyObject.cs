3a53c5bf678304df389af6597df3c0ed6f44152c
IsakMovitz <46811049+IsakMovitz@users.noreply.github.com>
2020-11-22 14:00:33 +0100

    Add files via upload

---
+++ Assets/DestroyObject.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyObject : MonoBehaviour
+{
+   
+    
+    void OnMouseDown()
+    {
+        Destroy(gameObject);
+        
+    }
+}
